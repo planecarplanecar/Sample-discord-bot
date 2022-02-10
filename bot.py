ec7276f0e85205b5d99ce6aadbd849df45164ac9
planecarplanecar <96955154+planecarplanecar@users.noreply.github.com>
2022-02-09 18:30:25 -0600

    Clarification

--- bot.py
+++ bot.py
@@ -4,3 +4,4 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='!')
 
 client.run('Paste your token here')
+#You have to paste your token from your bot application on the discord developer portal. Pasting this code won't work, but if you have a bot in a server, you should have an app ready.
